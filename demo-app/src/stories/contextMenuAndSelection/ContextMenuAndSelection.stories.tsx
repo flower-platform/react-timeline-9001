4be25ba7774535c9749bb03ce91dccdd2ba1ac52
Valeriu Iliescu <valeriu.iliescu@resonate-mp4.com>
2024-07-05 10:19:19 +0300

    #34317 - update modiff

--- demo-app/src/stories/contextMenuAndSelection/ContextMenuAndSelection.stories.tsx
+++ demo-app/src/stories/contextMenuAndSelection/ContextMenuAndSelection.stories.tsx
@@ -7,7 +7,6 @@ import { Timeline, IGanttAction, IGanttOnContextMenuShowParam, Item } from '@fam
 import { d, someHumanResources, someTasks } from '../sampleData';
 import { contextMenuScenarios, selectionScenarios } from './ContextMenuAndSelectionScenarios';
 import { Table, Column, DataCell } from 'fixed-data-table-2';
-import { ModalExt } from '../../../../../foundation-jhipster-gwt/foundation/foundation-ts/packages/scriptable-ui/dist-lib';
 
 export default {
     title: 'Features/Context Menu And Selection',
@@ -23,80 +22,80 @@ export const deleteActionIcon = "trash";
 export const deleteActionIconColor = "red";
 
 export const ContextMenu = () => {
-    const [tasks, setTasks] = useState<Item[]>([...someTasks]);
-    const [segmentHeight, setSegmentHeight] = useState<number>(40);
-    return (
-        <>
-            <Alert message={<><b>Add task</b> action is provided only for empty selection. It also has a different label depending on which row is displayed</>} />
-            <div style={{ display: 'flex', height: '400px' }}>
-                <Timeline startDate={d('2018-09-20')} endDate={d('2018-09-21')} groups={someHumanResources} items={tasks} itemHeight={segmentHeight}
-                    onContextMenuShow={(contextMenuShowParam: IGanttOnContextMenuShowParam) => {
-                        const actions: IGanttAction[] = [
-                            {
-                                icon: "edit",
-                                label: editActionLabel,
-                                isVisible: param => param.selection.length == 1,
-                                run: param => {
-                                    param.dontCloseContextMenuAfterRunAutomatically = true;
-                                    // This timeout is just for exemplify a delayed closing of the CM 
-                                    setTimeout(() => {
-                                        param.closeContextMenu();
-                                        const selectedTask = tasks.find((task) => task.key == param.selection[0]);
-                                        // This timeout for the menu to actual close before the prompt is shown
+        const [tasks, setTasks] = useState<Item[]>([...someTasks]);
+        const [segmentHeight, setSegmentHeight] = useState<number>(40);
+        return (
+            <>
+                <Alert message={<><b>Add task</b> action is provided only for empty selection. It also has a different label depending on which row is displayed</>}/>
+                <div style={{ display: 'flex', height: '400px' }}>
+                    <Timeline startDate={d('2018-09-20')} endDate={d('2018-09-21')} groups={someHumanResources} items={tasks} itemHeight={segmentHeight}
+                        onContextMenuShow={(contextMenuShowParam: IGanttOnContextMenuShowParam) => {
+                            const actions: IGanttAction[] = [
+                                {
+                                    icon: "edit",
+                                    label: editActionLabel,
+                                    isVisible: param => param.selection.length == 1,
+                                    run: param => {
+                                        param.dontCloseContextMenuAfterRunAutomatically = true;
+                                        // This timeout is just for exemplify a delayed closing of the CM 
                                         setTimeout(() => {
-                                            // TODO CS: se pare ca nu e o idee buna sa folosim prompt, fiindca blocheaza testul
-                                            // si nu putem simula scrierea.
-                                            // let newTitle = prompt("Task new title:", selectedTask.title);
-                                            let newTitle;
-                                            newTitle && setTasks(tasks.map((task) => task == selectedTask ? { ...task, title: newTitle } : task));
+                                            param.closeContextMenu();
+                                            const selectedTask = tasks.find((task) => task.key == param.selection[0]);
+                                            // This timeout for the menu to actual close before the prompt is shown
+                                            setTimeout(() => {
+                                                // TODO CS: se pare ca nu e o idee buna sa folosim prompt, fiindca blocheaza testul
+                                                // si nu putem simula scrierea.
+                                                // let newTitle = prompt("Task new title:", selectedTask.title);
+                                                let newTitle;
+                                                newTitle && setTasks(tasks.map((task) => task == selectedTask ? { ...task, title: newTitle } : task));
+                                            }, 10);
                                         }, 10);
-                                    }, 10);
+                                    }
+                                },
+                                {
+                                    isVisible: param => param.selection.length > 0 ,
+                                    /** This is a trivial example for customizing the content of an action renderer 
+                                     * but this content can be replaced according to application needs with a more complex one: 
+                                     * e.g. maybe containing a color picker (for an action that changes the color of a segment)*/
+                                    renderInMenu: (param) => {
+                                        return <Menu.Item onClick={() => { 
+                                                            setTasks(tasks.filter(task => !param.selection.includes(task.key)));
+                                                            param.closeContextMenu();
+                                                        }}>        
+                                                    <span style={{color: "red"}}>{deleteActionLabel}</span> 
+                                                    <Icon name={deleteActionIcon} color={deleteActionIconColor}/>      
+                                                </Menu.Item>
+                                    }
                                 }
-                            },
-                            {
-                                isVisible: param => param.selection.length > 0,
-                                /** This is a trivial example for customizing the content of an action renderer 
-                                 * but this content can be replaced according to application needs with a more complex one: 
-                                 * e.g. maybe containing a color picker (for an action that changes the color of a segment)*/
-                                renderInMenu: (param) => {
-                                    return <Menu.Item onClick={() => {
-                                        setTasks(tasks.filter(task => !param.selection.includes(task.key)));
-                                        param.closeContextMenu();
-                                    }}>
-                                        <span style={{ color: "red" }}>{deleteActionLabel}</span>
-                                        <Icon name={deleteActionIcon} color={deleteActionIconColor} />
-                                    </Menu.Item>
-                                }
-                            }
-                        ];
-                        // We can filter the actions that will be displayed directly here in the actions provider    
-                        if (contextMenuShowParam.actionParam.row < someHumanResources.length) {
-                            actions.splice(0, 0, {
-                                icon: <Icon name={addTaskActionIcon} />,
-                                label: param => addTaskActionLabel + someHumanResources[param.row].title,
-                                run: param => {
-                                    let end = moment(param.time);
-                                    end.hours(end.hours() + 3);
-                                    const maxKey = tasks.reduce((maxKey, task) => maxKey > (task.key as number) ? maxKey : (task.key as number), 0);
-                                    setTasks([...tasks, { key: maxKey + 1, row: someHumanResources[param.row].id, title: 'NEW TASK', start: param.time, end: end }]);
-                                }
-                            });
-                        } else {
-                            actions.splice(0, 0, { icon: addTaskActionIcon, label: () => addTaskNotPossibleAction });
-                        }
+                            ];
+                            // We can filter the actions that will be displayed directly here in the actions provider    
+                            if (contextMenuShowParam.actionParam.row < someHumanResources.length) {
+                                actions.splice(0, 0, {
+                                    icon: <Icon name={addTaskActionIcon} />,
+                                    label: param => addTaskActionLabel + someHumanResources[param.row].title,
+                                    run: param => { 
+                                        let end = moment(param.time); 
+                                        end.hours(end.hours() + 3);
+                                        const maxKey = tasks.reduce((maxKey, task) => maxKey > (task.key as number) ?  maxKey : (task.key as number), 0);
+                                        setTasks([...tasks, { key: maxKey + 1, row: someHumanResources[param.row].id, title: 'NEW TASK', start: param.time, end: end}]);
+                                    }
+                                });
+                            } else {
+                            	actions.splice(0, 0, { icon: addTaskActionIcon, label: () => addTaskNotPossibleAction});
+                        	}
 
-                        return actions;
-                    }}
-                    table={<Table width={100} >
-                        <Column
-                            columnKey="title"
-                            width={100}
-                            header={<DataCell>Title</DataCell>}
-                            cell={({ rowIndex }) => <DataCell>{rowIndex < someHumanResources.length ? someHumanResources[rowIndex].title : ""}</DataCell>} />
-                    </Table>}
-                />
-            </div>
-        </>);
+                            return actions;
+                        }} 
+                        table={<Table width={100} >
+                                    <Column
+                                        columnKey="title"
+                                        width={100}
+                                        header={<DataCell>Title</DataCell>}
+                                        cell={({rowIndex}) => <DataCell>{rowIndex < someHumanResources.length ? someHumanResources[rowIndex].title : ""}</DataCell>}/>
+                                </Table>}
+                        />
+                </div>
+            </>);
 };
 
 ContextMenu.parameters = {
@@ -110,36 +109,36 @@ export const selectionStoryTestIds = createTestids('SelectionStory', {
 });
 
 export const Selection = () => {
-    const [selectedItems, setSelectedItems] = useState<(number | string)[]>([]);
+    const [selectedItems, setSelectedItems] = useState<(number|string)[]>([]);
     const [isSelectionForced, setIsSelectionForced] = useState<boolean>(false);
     return (
-        <>
-            <span>
-                <Button toggle active={isSelectionForced} onClick={() => setIsSelectionForced(!isSelectionForced)}>
-                    Force selection programmatically
-                </Button>
-                (The user cannot change the selection via interaction)
-            </span>
-            <Alert message={<>Selected segments: <span data-testid={selectionStoryTestIds.selectedItemsSpan}>{selectedItems.sort().join(", ")}</span></>} />
-            {/* This is an example illustrates: 
+      <>
+        <span>
+            <Button toggle active={isSelectionForced} onClick={() => setIsSelectionForced(!isSelectionForced)}>
+                Force selection programmatically
+            </Button>
+            (The user cannot change the selection via interaction)
+        </span>
+        <Alert message={<>Selected segments: <span data-testid={selectionStoryTestIds.selectedItemsSpan}>{selectedItems.sort().join(", ")}</span></>}/>
+        {/* This is an example illustrates: 
             1.adding onSelectionChange handler 
             2.setting selectedItems property */}
-            <div style={{ display: 'flex', height: '400px' }}>
-                <Timeline startDate={d('2018-09-20')} endDate={d('2018-09-21')} groups={someHumanResources} items={someTasks}
+        <div style={{ display: 'flex', height: '400px' }}>
+          <Timeline startDate={d('2018-09-20')} endDate={d('2018-09-21')} groups={someHumanResources} items={someTasks} 
                     selectedItems={isSelectionForced ? [0, 1] : undefined} onSelectionChange={selectedItems => setSelectedItems(selectedItems)}
                     table={<Table width={100} >
-                        <Column
-                            columnKey="title"
-                            width={100}
-                            header={<DataCell>Title</DataCell>}
-                            cell={({ rowIndex }) => <DataCell>{rowIndex < someHumanResources.length ? someHumanResources[rowIndex].title : ""}</DataCell>} />
-                    </Table>} />
-            </div>
-        </>
+                                    <Column
+                                        columnKey="title"
+                                        width={100}
+                                        header={<DataCell>Title</DataCell>}
+                                        cell={({rowIndex}) => <DataCell>{rowIndex < someHumanResources.length ? someHumanResources[rowIndex].title : ""}</DataCell>}/>
+                                </Table>}/>
+        </div>
+      </>
     );
-};
+  };
 
-Selection.parameters = {
+  Selection.parameters = {
     scenarios: [
         ...Object.keys(selectionScenarios).map(key => selectionScenarios[key])
     ]
